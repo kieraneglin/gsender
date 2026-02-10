29ac8b87f93daf0c7ae5e3822b5bc3f3225d411d
kgn <kevin.glovernetherton@gmail.com>
2026-02-10 11:08:40 -0500

    Unit badge updates

--- src/app/src/features/DRO/component/UnitBadge.tsx
+++ src/app/src/features/DRO/component/UnitBadge.tsx
@@ -31,11 +31,11 @@ export function UnitBadge({ isRemote }: Props) {
                 onPointerUp={isRemote ? () => setShowPopup(true) : () => {}}
                 className="z-10 absolute -top-2 -left-1 max-xl:top-0 max-xl:-left-1 px-2 max-xl:px-1 py-1.5 max-xl:py-1 text-xs font-semibold text-gray-600 bg-gray-300 rounded-tl items-center text-center rounded-br-lg  dark:bg-gray-700 dark:text-gray-400"
             >
-                <div className="2xl:block xl:hidden max-xl:hidden">
+                <div className="max-xl:hidden">
                     Units:
                     <br /> {units}
                 </div>
-                <div className={"2xl:hidden xl:block p-1"}>
+                <div className={"max-xl:block hidden p-1"}>
                     {unitLabel}
                 </div>
             </div>
