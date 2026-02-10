9779544d0f27c620b96d450038c1feb898cee45f
Chris Thorogood <chris_thorogood@sienci.com>
2026-02-10 14:01:54 -0500

    Add max height to Probe component grid

--- src/app/src/features/Probe/Probe.tsx
+++ src/app/src/features/Probe/Probe.tsx
@@ -127,7 +127,7 @@ const Probe = ({ state, actions }: ProbeProps) => {
 
     return (
         <div className="w-full h-full max-xl:pt-2">
-            <div className="grid grid-cols-[5fr_3fr] w-full h-full">
+            <div className="grid grid-cols-[5fr_3fr] w-full h-full max-xl:max-h-[144px]">
                 {/* <div className="w-full h-full m-auto grid gap-4">
                     <div className="h-full grid grid-rows[4fr_2fr] self-center gap-2"> */}
                 <div className="grid grid-rows-[1fr_1fr_1fr] gap-2 items-center justify-center">
