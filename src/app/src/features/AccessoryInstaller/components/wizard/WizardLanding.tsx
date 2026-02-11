756c8d9cce3b56fcb6cae92be579522cc8ab3483
KGN <kevin.glovernetherton@gmail.com>
2026-02-11 10:38:31 -0500

    Always enabled second steps again

--- src/app/src/features/AccessoryInstaller/components/wizard/WizardLanding.tsx
+++ src/app/src/features/AccessoryInstaller/components/wizard/WizardLanding.tsx
@@ -71,7 +71,7 @@ export function WizardLanding({
                     <div className="flex flex-col gap-3 mt-12 max-w-md">
                         {subWizards.map((subWizard, index) => {
                             const isActive = index === 0;
-                            const isDisabledBySequence = index > 0;
+                            const isDisabledBySequence = index < 0;
                             const isDisabled =
                                 validationsFailed || isDisabledBySequence;
 
