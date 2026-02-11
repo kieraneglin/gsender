921205e99c458c529456996b856fc80e71be9c30
kgn <kevin.glovernetherton@gmail.com>
2026-02-11 10:21:40 -0500

    Added wrapper component

---
+++ src/app/src/features/AccessoryInstaller/Wizards/atc/components/ATCConfigStep.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { StepProps } from 'app/features/AccessoryInstaller/types';
+import controller from 'app/lib/controller.ts';
+import { ConfigTab } from 'app/features/ATC/components/Configuration/components/ConfigTab.tsx';
+import { ConfigProvider, useConfigContext } from 'app/features/ATC/components/Configuration/hooks/useConfigStore.tsx';
+import { repopulateFromSDCard } from 'app/features/ATC/components/Configuration/utils/ConfigUtils.ts';
+
+function ATCConfigStepContent() {
+    const { updateConfig } = useConfigContext();
+    const [uploading, setUploading] = useState(false);
+
+    useEffect(() => {
+        const handleSdcardJson = (payload) => {
+            const updatedConfig = repopulateFromSDCard(payload.code);
+            updateConfig({
+                variables: { ...updatedConfig.variables },
+            });
+        };
+        const handleYmodemStart = () => setUploading(true);
+        const handleYmodemComplete = () => setUploading(false);
+        const handleYmodemError = () => setUploading(false);
+
+        controller.addListener('sdcard:json', handleSdcardJson);
+        controller.addListener('ymodem:start', handleYmodemStart);
+        controller.addListener('ymodem:complete', handleYmodemComplete);
+        controller.addListener('ymodem:error', handleYmodemError);
+        controller.command('sdcard:read', 'ATCI.macro');
+
+        return () => {
+            controller.removeListener('sdcard:json', handleSdcardJson);
+            controller.removeListener('ymodem:start', handleYmodemStart);
+            controller.removeListener('ymodem:complete', handleYmodemComplete);
+            controller.removeListener('ymodem:error', handleYmodemError);
+        };
+    }, [updateConfig]);
+
+    return <ConfigTab uploading={uploading} />;
+}
+
+export function ATCConfigStep(_props: StepProps) {
+    return (
+        <ConfigProvider>
+            <ATCConfigStepContent />
+        </ConfigProvider>
+    );
+}
