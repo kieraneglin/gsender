f7c1a66fb0ec400768af0c7afe5e22fc13e675e0
walidkayhan <walid.kayhan@hotmail.com>
2026-02-23 10:18:35 -0500

    Remove gamepad and keyboard icon links from topbar

--- src/app/src/features/StatusIcons/index.tsx
+++ src/app/src/features/StatusIcons/index.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react';
 import cx from 'classnames';
-import { Link } from 'react-router';
-import { LuGamepad2 } from 'react-icons/lu';
-import { FaRegKeyboard } from 'react-icons/fa6';
 
 import { RemoteModeDialog } from 'app/features/RemoteMode';
 import actions, {
@@ -12,7 +9,6 @@ import RemoteIndicator from 'app/features/RemoteMode/components/RemoteIndicator.
 import Tooltip from 'app/components/Tooltip';
 
 const StatusIcons = () => {
-    const [gamepadConnected, setGamePadConnected] = useState(false);
     const [headlessSettings, setHeadlessSettings] = useState<HeadlessSettings>({
         ip: '',
         port: 0,
@@ -27,31 +23,6 @@ const StatusIcons = () => {
 
     useEffect(() => {
         actions.fetchSettings(setHeadlessSettings);
-
-        const gameConnectHandler = () => {
-            const gamepads = navigator.getGamepads();
-            const hasGamepad = gamepads.some((gamepad) => gamepad !== null);
-
-            setGamePadConnected(hasGamepad);
-        };
-
-        const gameDisconnectHandler = () => {
-            const gamepads = navigator.getGamepads();
-            const hasGamepad = gamepads.some((gamepad) => gamepad !== null);
-
-            setGamePadConnected(hasGamepad);
-        };
-
-        window.addEventListener('gamepadconnected', gameConnectHandler);
-        window.addEventListener('gamepaddisconnected', gameDisconnectHandler);
-
-        return () => {
-            window.removeEventListener('gamepadconnected', gameConnectHandler);
-            window.removeEventListener(
-                'gamepaddisconnected',
-                gameDisconnectHandler,
-            );
-        };
     }, []);
 
     return (
@@ -69,27 +40,7 @@ const StatusIcons = () => {
                     />
                 </button>
             </Tooltip>
-            <Tooltip content="Keyboard Shortcuts">
-                <Link
-                    className="flex flex-col gap-0.5  self-center content-center items-center justify-center text-sm text-gray-500"
-                    to={'/tools/keyboard-shortcuts'}
-                >
-                    <FaRegKeyboard className="text-green-500 w-7 h-7" />
-                </Link>
-            </Tooltip>
-            <Tooltip content="Gamepad Shortcuts">
-                <Link
-                    className="flex flex-col gap-0.5  self-center content-center items-center justify-center text-sm text-gray-500"
-                    to={'/tools/gamepad'}
-                >
-                    <LuGamepad2
-                        className={cx('w-7 h-7', {
-                            'text-gray-400': !gamepadConnected,
-                            'text-green-500': gamepadConnected,
-                        })}
-                    />
-                </Link>
-            </Tooltip>
+            
             <RemoteModeDialog
                 showRemote={showRemoteDialog}
                 onClose={() => setShowRemoteDialog(false)}
